f786cedfef71a2335bc44416de473f8171bb4a9d
Baptiste Fontaine <b@ptistefontaine.fr>
2019-03-23 18:28:47 +0100

    bench clygments with clython

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clygments "2.0.0"]
+                 [clygments "2.0.0-clython-SNAPSHOT"]
                ; [clygments "1.0.0"]
                  [com.oscaro/tools-io "0.3.16"]])
